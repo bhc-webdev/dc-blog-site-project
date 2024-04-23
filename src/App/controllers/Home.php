99f54193ca2fe5943b7089a433ec217db94cf545
MarcmotenDC <superman868789@gmail.com>
2024-04-23 12:10:18 -0500

    Connect to blogs table

--- src/App/controllers/Home.php
+++ src/App/controllers/Home.php
@@ -2,18 +2,29 @@
 
 namespace App\Controllers;
 
+use App\Models\Blog;
 use Framework\Viewer;
 
 class Home
 {
     public function index()
     {
+        $model = new Blog;
+        $blogs = $model->getData();
+
+        if (!$blogs) {
+
+            exit("Blog not found");
+            
+        }
         $viewer = new Viewer;
 
         echo $viewer->render("shared/header.php", [
             "title" => "Home"
         ]);
 
-        echo $viewer->render("Home/index.php");
+        echo $viewer->render("Home/index.php",  [
+            "blogs" => $blogs
+        ]);
     }
 }
