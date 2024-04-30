ea5f68f97ffbced862a5ace57be270aed227af81
MarcmotenDC <superman868789@gmail.com>
2024-04-30 12:43:42 -0500

    Move Navbar

---
+++ views/shared/nav.php
@@ -0,0 +1,25 @@
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary blueGradientBackground" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">All Things Dallas College</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blogs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
